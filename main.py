608eb4739929a36af5ee320c16f420be39a13e4e
BaAniq <basia.anisimowicz@gmail.com>
2023-11-17 22:08:48 +0100

    Adding labels and buttons

--- main.py
+++ main.py
@@ -10,4 +10,28 @@ image_logo = PhotoImage(file='logo.png')
 set_background_image.create_image(100, 95, image=image_logo)
 set_background_image.grid(row=1, column=2)
 
+website_label = Label(text='Website:', font=('Arial', 10), fg='black')
+website_label.grid(row=2, column=1)
+
+email_label = Label(text='Email/Username:', font=('Arial', 10), fg='black')
+email_label.grid(row=3, column=1)
+
+password_label = Label(text='Password:', font=('Arial', 10), fg='black')
+password_label.grid(row=4, column=1)
+
+website_textbox = Entry(width=30)
+website_textbox.grid(row=2, column=2)
+
+email_textbox = Entry(width=30)
+email_textbox.grid(row=3, column=2)
+
+password_textbox = Entry(width=30)
+password_textbox.grid(row=4, column=2)
+
+generate_password_button = Button(text='Generate Password')
+generate_password_button.grid(row=4, column=3)
+
+add_button = Button(text='Add')
+add_button.grid(row=5, column=2)
+
 window.mainloop()
