e049e8548a8633105210638c311680fcd3bde51d
BaAniq <basia.anisimowicz@gmail.com>
2023-11-17 21:53:46 +0100

    Adding background image

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tkinter import *
 
 window = Tk()
+window.config(padx=50, pady=50)
 window.title('Your password manager')
 window.minsize(height=300, width=500)
 
+set_background_image = Canvas(width=200, height=189, highlightthickness=0)
+image_logo = PhotoImage(file='logo.png')
+set_background_image.create_image(100, 95, image=image_logo)
+set_background_image.grid(row=1, column=2)
 
 window.mainloop()
