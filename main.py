4856f2592c445e8c15c154ea2b01a8aa59ce633c
BaAniq <basia.anisimowicz@gmail.com>
2023-11-18 10:43:57 +0100

    Improve the generate a password part of code

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from tkinter import *
 from tkinter import messagebox
-
+from random import randint, choice, shuffle
 # ---------------------_GENERATE A PASSWORD_---------------------
-import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_numbers = random.randint(2, 4)
-nr_symbols = random.randint(2, 4)
+nr_letters = randint(8, 10)
+nr_numbers = randint(2, 4)
+nr_symbols = randint(2, 4)
 
-password_list = []
-for element in range(nr_letters):
-    password_list.append(random.choice(letters))
+password_letters = [choice(letters) for x in range(nr_letters)]
+password_numbers = [choice(numbers) for x in range(nr_numbers)]
+password_symbols = [choice(symbols) for x in range(nr_symbols)]
 
-for element in range(nr_numbers):
-    password_list.append(random.choice(numbers))
+password_list = password_letters+password_numbers+password_symbols
 
-for element in range(nr_symbols):
-    password_list.append(random.choice(symbols))
-
-random.shuffle(password_list)
+shuffle(password_list)
 
 generated_password = ''.join(password_list)
 
